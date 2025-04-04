32e41d76b3602ad11e18c4c8211281a95391d7de
JBratvold <mcbratjo@gmail.com>
2025-04-04 08:32:29 -0600

    Add red and text when item is not found in market listings instead of crashing

--- utils/cost_calculator.py
+++ utils/cost_calculator.py
@@ -1,7 +1,8 @@
 # cost_calculator.py
 
 from utils.market_api import get_market_price
-from config import SHIP_BLUEPRINTS
+from config import SHIP_BLUEPRINTS,TYPE_NAMES
+from utils.filters import get_type_name
 
 def calculate_total_cost(ship_name, lp_to_isk_ratio):
     """Calculates total cost for a ship, including materials and fixed costs."""
@@ -15,7 +16,12 @@ def calculate_total_cost(ship_name, lp_to_isk_ratio):
     for type_id, quantity in ship["materials"].items():
         price_per_unit = get_market_price(type_id)
         if price_per_unit is None:
-            return None, f"Failed to get price for material {type_id}"
+            materials_cost[type_id] = {
+                "price_per_unit": None,
+                "quantity": quantity,
+                "total_price": None
+            }
+            continue  # Skip adding to total cost
 
         total_price = price_per_unit * quantity
         materials_cost[type_id] = {
